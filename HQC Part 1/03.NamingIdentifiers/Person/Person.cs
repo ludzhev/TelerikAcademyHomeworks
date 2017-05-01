ee7bf405d79800efea381477e72e479107cc2039
Stefan Ludzhev <ludzhev@gmail.com>
2017-05-01 19:27:22 +0300

    Naming identifiers homework.

---
+++ HQC Part 1/03.NamingIdentifiers/Person/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace Person
+{
+    public class PersonFactory
+    {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public Person GetHuman(int age)
+        {
+            Person currentPerson = new Person { Age = age };
+
+            if (age % 2 == 0)
+            {
+                currentPerson.Name = "Adam";
+                currentPerson.Gender = Gender.Male;
+            }
+            else
+            {
+                currentPerson.Name = "Eve";
+                currentPerson.Gender = Gender.Female;
+            }
+
+            return currentPerson;
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public Gender Gender { get; set; }
+        }
+    }
+}
---
+++ HQC Part 1/03.NamingIdentifiers/VariableLogger/StartUP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariableLogger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            VariableConsoleLogger variableLogger = new VariableConsoleLogger();
+            variableLogger.Log(true);
+        }
+
+        public class VariableConsoleLogger
+        {
+            public void Log(bool variable)
+            {
+                string variableAsString = variable.ToString();
+                Console.WriteLine(variableAsString);
+            }
+        }
+    }
+}
